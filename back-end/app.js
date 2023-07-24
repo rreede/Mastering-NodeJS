faff29aa1403a77360ff01e779cb4d0d2b895f49
Rene Reede <rene.reede@gmail.com>
2023-07-24 10:49:45 +0300

    Added new views and redered with ejs

--- back-end/app.js
+++ back-end/app.js
@@ -15,3 +15,26 @@ app.get('/', (req,res)=>{
     //res.send('<p>Home Page</p>');
     res.render('index');
 });
+
+app.get('/about', (req,res)=>{
+    //res.send('<p>Home Page</p>');
+    res.render('about');
+});
+
+app.get('/blogs/create', (req,res)=> {
+
+    res.render(create.ejs);
+
+});
+
+/*
+app.get('/about-us', (req,res)=>{
+    //res.send('<p>Home Page</p>');
+    res.redirect('about');
+});
+
+*/
+
+app.use((req,res) => {
+    res.status(404).render('404');
+});
